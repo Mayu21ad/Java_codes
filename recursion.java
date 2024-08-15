b897f3573a88c55cd6dd64139472566fb5e637bd
Mayuri G <105342477+Mayu21ad@users.noreply.github.com>
2024-08-15 18:49:17 +0530

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,15 @@
+public class recursion{
+    public static void main(String[] args){
+        int n = 10;
+        printer(n);
+    }
+    
+    public static void printer(int n){
+        if (n == 0){
+            return;
+        }
+        System.out.print(n + " ");
+        printer(n - 1);
+        System.out.print(n + " ");
+    }
+}
