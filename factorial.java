bc000132eb76203bb6f76c5b7b2e5f054aaf8b97
Mayuri G <105342477+Mayu21ad@users.noreply.github.com>
2024-08-15 19:07:56 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+class factorial{
+    static long fact(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+
+        return n * fact(n-1);
+    }
+
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(fact(n));
+    }
+}
